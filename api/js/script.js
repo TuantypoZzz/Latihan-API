4cee4d4fa648a4d4e7408f86fa37d0206bc2a58c
siesta.staff <staff@siesta.id>
2022-05-14 14:53:37 +0700

    FIX: modal not shown. UPDATE : modal data

--- api/js/script.js
+++ api/js/script.js
@@ -17,13 +17,33 @@ function cariFilm(){
                     $('#list-filem').append(`
                     <div class="col-md-4">
                         <div class="card mb-3">
-                        <img src="`+data.Poster+`" class="card-img-top" alt="...">
+                        <img src="${data.Poster}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">"`+data.Title+`"</h5>
                             <h6 class="card-subtitle mb-2 text-muted">"`+data.Year+`"</h6>
-                            <a href="#" class="card-link" data-bs-toggle="modal" data-bs-target="#exampleModal">See Detail</a>
+                            <a href="#" class="card-link" data-toggle="modal" data-target="#movieDetail${i}">See Detail</a>
                         </div>
                         </div>
+                    </div>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="movieDetail${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <img src="${data.Poster}" class="card-img-top" alt="...">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                        </div>
+                    </div>
                     </div>
                     `)
                 })
@@ -32,7 +52,7 @@ function cariFilm(){
 
 
             } else {
-                $('#list-filem').html(`<h1 class="text-center">'+ hasil.Error +'</h1>
+                $('#list-filem').html(`<h1 class="text-center">'`+ hasil.Error +`'</h1>
                 
                 `)
             }
